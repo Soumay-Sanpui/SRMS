878c0e1034421917b617564c7831197a431a462d
Soumay-Sanpui <somusanpui9@gmail.com>
2024-09-21 15:20:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1 className="text-center text-2xl font-bold">Hello Somu</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
